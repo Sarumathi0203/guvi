fc0c3718efbb8e4f8dfb1a5df788bd43bf585608
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2018-09-17 15:56:40 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,19 @@
+//PROGRAM TO FIND ODD OR EVEN
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter a number:\n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("no. is even!");
+}
+else
+{
+printf("no. is odd!\n");
+}
+getch();
+}
